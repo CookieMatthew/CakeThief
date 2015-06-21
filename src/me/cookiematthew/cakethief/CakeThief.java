c9fb1fcda83fd796c0ba298383e58b20e8462181
shane7712 <shane7712@hotmail.com>
2015-06-21 09:50:35 -0500

    First commit, nearly done with plugin.

---
+++ src/me/cookiematthew/cakethief/CakeThief.java
@@ -0,0 +1,24 @@
+package me.cookiematthew.cakethief;
+
+import me.cookiematthew.cakethief.listeners.player.AsyncPlayerPreLogin;
+import me.cookiematthew.cakethief.listeners.player.PlayerJoin;
+import me.cookiematthew.cakethief.listeners.player.PlayerQuit;
+import me.cookiematthew.cakethief.threads.StartCountdown;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CakeThief extends JavaPlugin{
+
+    public void onEnable(){
+        GameState.setState(GameState.IN_LOBBY);
+        new Thread(new StartCountdown()).start();
+    }
+
+    public void registerListeners(){
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new PlayerJoin(this), this);
+        pm.registerEvents(new PlayerQuit(this), this);
+        pm.registerEvents(new AsyncPlayerPreLogin(this), this);
+    }
+
+}
